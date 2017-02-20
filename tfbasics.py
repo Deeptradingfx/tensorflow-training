aa47e6a81de32106cf0fedd205ac8c00ece5d561
AFAgarap <abienfred.agarap@yahoo.com>
2017-02-20 17:24:46 +0800

    Basics of Computational graph

--- tfbasics.py
+++ tfbasics.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 x1 = tf.constant(5)
 x2 = tf.constant(6)
 
-result = tf.mul(x1, x2)
+result = tf.multiply(x1, x2)
 
 print(result)
 
@@ -19,3 +19,26 @@ with tf.Session() as sess:
 
 print(output)
 
+a = tf.constant(3.0, tf.float32)
+b = tf.constant(4.0) # implicit tf.float32
+
+print(a, b)
+
+with tf.Session() as sess:
+        print(sess.run([a, b]))
+        c = tf.add(a, b)
+        print('c:', c)
+        print('sess.run(c):', sess.run(c))
+
+node1 = tf.placeholder(tf.float32)
+node2 = tf.placeholder(tf.float32)
+adder_node = node1 + node2 # shortcut for tf.add(node1, node2)
+
+with tf.Session() as sess:
+        print(sess.run(adder_node, {node1: 3, node2: 4.5}))
+        print(sess.run(adder_node, {node1: [1, 3], node2: [2, 4]}))
+
+add_and_triple = adder_node * 3
+
+with tf.Session() as sess:
+        print(sess.run(add_and_triple, {node1: 3, node2: 4.5}))
