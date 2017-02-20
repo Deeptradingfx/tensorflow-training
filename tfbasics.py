8c78e3d457e2282d90440c4ea19ade22061c0766
AFAgarap <abienfred.agarap@yahoo.com>
2017-02-20 17:39:13 +0800

    Basic Computational graph showing Perceptron training

--- tfbasics.py
+++ tfbasics.py
@@ -35,6 +35,7 @@ node2 = tf.placeholder(tf.float32)
 adder_node = node1 + node2 # shortcut for tf.add(node1, node2)
 
 with tf.Session() as sess:
+        # evaluate the graph with multiple inputs using feed_dict parameter
         print(sess.run(adder_node, {node1: 3, node2: 4.5}))
         print(sess.run(adder_node, {node1: [1, 3], node2: [2, 4]}))
 
@@ -42,3 +43,25 @@ add_and_triple = adder_node * 3
 
 with tf.Session() as sess:
         print(sess.run(add_and_triple, {node1: 3, node2: 4.5}))
+
+W = tf.Variable([.3], tf.float32)
+b = tf.Variable([-.3], tf.float32)
+x = tf.placeholder(tf.float32)
+linear_model = W * x + b
+y = tf.placeholder(tf.float32)
+squared_deltas = tf.square(linear_model - y)
+loss = tf.reduce_sum(squared_deltas)
+
+with tf.Session() as sess:
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
+    print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+
+fixW = tf.assign(W, [-1.])
+fixB = tf.assign(b, [1.])
+
+with tf.Session() as sess:
+    sess.run([fixW, fixB])
+    print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
+    print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
