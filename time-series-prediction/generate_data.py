98647723f82af507207687634c3d60c114dd438e
AFAgarap <abienfred.agarap@yahoo.com>
2017-08-07 20:14:44 +0800

    Fixed the np.arrange() --> np.arange()

--- time-series-prediction/generate_data.py
+++ time-series-prediction/generate_data.py
@@ -5,5 +5,5 @@ SEQ_LEN = 10
 def create_time_series():
     freq = (np.random.random() * 0.5) + 0.1 
     ampl = np.random.random() + 0.5
-    x = np.sin(np.arrange(0, SEQ_LEN) * freq) * ampl
+    x = np.sin(np.arange(0, SEQ_LEN) * freq) * ampl
     return x
