9f643b0e5c63309342411c168ef4e1d526181adf
AFAgarap <abienfred.agarap@yahoo.com>
2017-08-30 21:11:43 +0800

    OOP implementation of Logistic Regression for MNIST Classification

--- getting-started/mnist.py
+++ getting-started/mnist.py
@@ -1,35 +1,92 @@
-from tensorflow.examples.tutorials.mnist import input_data
+"""Logistic Regression for MNIST Classification"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__version__ = '0.1'
+__author__ = 'Abien Fred Agarap'
+
+import argparse
 import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import sys
 
-mnist = input_data.read_data_sets('MNIST_data/', one_hot=True)
 
-x = tf.placeholder(tf.float32, [None, 784])
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
-# because of usage of tf.nn.softmax_cross_entropy_with_logits()
-# y = tf.nn.softmax(tf.matmul(x, W) + b)
-y = tf.matmul(x, W) + b
+class LogisticRegression:
 
-y_ = tf.placeholder(tf.float32, [None, 10])
+    def __init__(self, data_input):
+        self.data_input = data_input
 
-# this is numerically unstable though
-# cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+        def __graph__():
+            x = tf.placeholder(tf.float32, [None, 784], name='x_input')
+            y = tf.placeholder(tf.float32, [None, 10], name='actual_label')
+            w = tf.Variable(tf.zeros([784, 10]), name='weights')
+            b = tf.Variable(tf.zeros([10]), name='biases')
 
-# use the following instead
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+            # numerically unstable version
+            # y = tf.nn.softmax(tf.matmul(x, W) + b)
+            # cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
-train = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+            y_ = tf.matmul(x, w) + b
+            y_ = tf.identity(y_, name='logits')
 
-init = tf.global_variables_initializer()
+            predictions = tf.nn.softmax(y, name='predictions')
 
-with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
-    with tf.device('/gpu:0'):
-        sess.run(init)
+            cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=y_))
+            train_op = tf.train.GradientDescentOptimizer(learning_rate=0.5).minimize(cross_entropy)
 
-        for _ in range(1000):
-            batch_xs, batch_ys = mnist.train.next_batch(100)
-            sess.run(train, feed_dict={x: batch_xs, y_: batch_ys})
+            correct_prediction = tf.equal(tf.argmax(y_, 1), tf.argmax(y, 1))
+            accuracy_op = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_:mnist.test.labels}))
+            self.x = x
+            self.w = w
+            self.b = b
+            self.y = y
+            self.predictions = predictions
+            self.y_ = y_
+            self.cross_entropy = cross_entropy
+            self.train_op = train_op
+            self.accuracy_op = accuracy_op
+
+        sys.stdout.write('\n<log> Building graph...')
+        __graph__()
+        sys.stdout.write('</log>\n')
+
+    def train(self):
+        init = tf.global_variables_initializer()
+
+        with tf.Session() as sess:
+            sess.run(init)
+
+            for step in range(1000):
+                batch_xs, batch_ys = self.data_input.train.next_batch(100)
+
+                feed_dict = {self.x: batch_xs, self.y: batch_ys}
+
+                _, loss, accuracy = sess.run([self.train_op, self.cross_entropy, self.accuracy_op], feed_dict=feed_dict)
+
+                if step % 100 == 0:
+                    print('step [{}] -- loss: {}, accuracy: {}'.format(step, loss, accuracy))
+
+            feed_dict = {self.x: self.data_input.test.images, self.y: self.data_input.test.labels}
+
+            test_accuracy = sess.run(self.accuracy_op, feed_dict=feed_dict)
+            print('Test Accuracy: {}'.format(test_accuracy))
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Logistic Regression for MNIST Classification')
+    group = parser.add_argument_group('Arguments')
+    group.add_argument('-d', '--dataset', required=True, type=str,
+                       help='path of the MNIST dataset')
+    arguments = parser.parse_args()
+    return arguments
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    mnist = input_data.read_data_sets(args.dataset, one_hot=True)
+
+    model = LogisticRegression(data_input=mnist)
+    model.train()
