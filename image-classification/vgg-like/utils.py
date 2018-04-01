39e0a9fd735dfded57358dca69640781eb923ebc
AFAgarap <abienfred.agarap@yahoo.com>
2018-04-02 02:00:15 +0800

    chore : add utility module

---
+++ image-classification/vgg-like/utils.py
@@ -0,0 +1,37 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__version__ = '0.1.0'
+__author__ = 'Abien Fred Agarap'
+
+from keras.utils import to_categorical
+import numpy as np
+import os
+from PIL import Image
+from sklearn.preprocessing import LabelEncoder
+
+
+def load_dataset(dataset_path, one_hot=False):
+
+    files_list = []
+
+    for subdirectories, directories, files in os.walk(dataset_path):
+        for file in files:
+            if file.endswith('.jpg') or file.endswith('.jpeg'):
+                files_list.append(os.path.join(subdirectories, file))
+
+    images = np.array([[np.asarray(Image.open(file).resize((32, 32))), os.path.dirname(file).strip('./')] for file in files_list])
+
+    features = images[:, 0]
+    features = np.array([feature for feature in features])
+
+    labels = images[:, 1]
+    labels = LabelEncoder().fit_transform(labels)
+
+    del images
+
+    if one_hot:
+        labels = to_categorical(labels)
+
+    return features, labels
